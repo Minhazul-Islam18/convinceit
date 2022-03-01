b6a85982a74c6169b21b6071f5a25abb40d12c78
Minhazul-Islam18 <minhaz019166@gmail.com>
2022-03-01 21:39:15 +0600

    twak

--- resources/views/layouts/loader.blade.php
+++ resources/views/layouts/loader.blade.php
@@ -1031,6 +1031,19 @@
         <script src="{{ asset('website') }}/translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
         <script src="{{ asset('website') }}/js/color-switcher.js"></script>
     </div>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/621e34761ffac05b1d7c6ba4/1ft2vpsnj';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
